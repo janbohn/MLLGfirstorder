b763a046ab71d5f7c02245427967a579841e701c
Jan Bohn <108763979+janbohn@users.noreply.github.com>
2022-07-06 16:16:51 +0200

    Add files via upload

--- MLLGfuncsparse3.py
+++ MLLGfuncsparse3.py
@@ -7,8 +7,8 @@ import bempp.api
 import matplotlib.pyplot as plt
 from scipy.special import comb
 from scipy.sparse.linalg.interface import aslinearoperator
-from scipy.sparse.linalg import gmres
-#from bempp.api.linalg.iterative_solvers import gmres
+from scipy.sparse.linalg import *#gmres,spsolve
+#from bempp.api.linalg.iterative_solvers import *
 from bempp.api.external.fenics import FenicsOperator
 #from bempp.api import as_matrix
 from maxwellrt0 import *
@@ -16,6 +16,7 @@ from bempp.api.operators.boundary import maxwell
 import timeit
 import sys
 from bempp.api.assembly.blocked_operator import BlockedDiscreteOperator
+import scipy
 
 sys.setrecursionlimit(100000)
 if not has_linear_algebra_backend("PETSc"):
@@ -26,33 +27,29 @@ if not has_slepc():
     exit()
 
 
-def MLLGfuncsparse(T, N, tau, h, m, rho, L, tolgmres, eps, mu, sig, theta, alpha, Ce):  # ,J,m0,E0,H0):
+def MLLGfuncsparse(T, N, tau, h, m, rho, L, tolgmres, eps, mu, sig, theta, alpha, Ce,thick):  # ,J,m0,E0,H0):
     print('h is ', h, ' N is', N, 'ich bin 3')
-    onof= 0.0 
-    refadd=0.0
-    exSolutionCode = ("-1*(pow(x[0],3)-0.75*x[0])*sin(multF*M_PI*t/tend)","sqrt(1-1*pow(pow(x[0],3)-0.75*x[0],2))","-1*(pow(x[0],3)-0.75*x[0])*cos(multF*M_PI*t/tend)")
-    dtexSolutionCode = ("-1*(pow(x[0],3)-0.75*x[0])*cos(multF*M_PI*t/tend)*multF*M_PI/tend","0","1*(pow(x[0],3)-0.75*x[0])*sin(multF*M_PI*t/tend)*multF*M_PI/tend")
-    class MyExpressionH(UserExpression):
-        def eval(self, value, x):
-            s1=sin(np.pi*x[0])
-            s2=sin(np.pi*x[1])
-            s3=sin(np.pi*x[2])
-            s1s=s1**2
-            s2s=s2**2
-            s3s=s3**2
-            value[0] = 0
-            value[1] =-2*np.pi*s1s*s2s*s3*cos(np.pi*x[2])  /mu
-            value[2] = 2*np.pi*s1s*s2*cos(np.pi*x[1])*s3s   /mu
-        def value_shape(self):
-            return (3,)    
     
+    onof= 1.0 # with onof= 0.0 you can simulate the uncoupled system. 
+    refadd=2.0 # no exact solution given. with refadd=0.0 you can compare the approximation to a predefined solution given via dtMex (time derivative of exact solution) and Mex (exact solution) 
+    ps=1 # degree of FEM and BEM spaces. (only ps=1 is implemented in BEMPP) 
     
-    (Lhs,rhsLLGH, M0,mj,Ej,Hj,JccE,JE,nBEM,nFEM,nMAG,X,trace_space,VV,V3,wei,boundary_rhs,dtmfunc)=assLHS( tau, h, m, eps, mu, sig,rho,L,N)
-    mj=interpolate(Expression(exSolutionCode,multF=3,tend=10*T,t=0.0,degree=1),V3)
-    Hex=interpolate(MyExpressionH(degree=1), X)
+    ### plots
+    vtkfilem = File('plots/mfield.pvd') 
+    vtkfileE = File('plots/Efield.pvd')
+    vtkfileH = File('plots/Hfield.pvd')
+    vtkfilePhi = File('plots/phifield.pvd')
+    vtkfilePsi = File('plots/psifield.pvd')
+    
+      
+    
+    # matrices for discretization and input data 
+    (Lhs,rhsLLGH, M0,mj,Ej,Hj,JccE,JE,nBEM,nFEM,nMAG,X,trace_space,VV,V3,wei,boundary_rhs,dtmfunc) =assign_data_and_systems( tau, h, m, eps, mu, sig,rho,L,N,thick)
+    # external magnetic field
+    Hex = project(Expression(['0.0', '-10.0', '0.0'], degree=1), V3)
     
     # Coefficient vector [E,H,phi,psi]
-    sol = np.concatenate([Ej.vector(), Hj.vector(), np.zeros(nBEM),np.zeros(nBEM)])
+    sol = np.concatenate([Ej.vector()[:], Hj.vector()[:], np.zeros(nBEM),np.zeros(nBEM)])
     randkoeff = np.zeros([int(N + 1), 2 * nBEM])  # storage variable for boundary coefficients
     dtmpsiko = np.zeros([int(N + 1), 2 * nBEM])
     mkoeff = np.zeros([int(N + 1), nMAG])
@@ -63,7 +60,15 @@ def MLLGfuncsparse(T, N, tau, h, m, rho, L, tolgmres, eps, mu, sig, theta, alpha
     Ekoeff[0, :] = np.real(Ej.vector())
     Hkoeff[0, :] = np.real(Hj.vector())
     mkoeff[0, :] = np.real(mj.vector())
-
+    
+    #variables for plot 
+    mplot= interpolate(Expression(['0.00', '0.0', '0.0'], degree=ps), V3)
+    Hplot= interpolate(Expression(['0.00', '0.0', '0.0'], degree=ps), X)
+    Eplot= interpolate(Expression(['0.00', '0.0', '0.0'], degree=ps), X)
+    #Phiplot = bempp.api.GridFunction(trace_space, coefficients=psikoeff[1,:])
+    #Psiplot = bempp.api.GridFunction(trace_space, coefficients=psikoeff[1,:])
+    
+    
     ########## Time Stepping ##################################################################
 
     for j in range(0, int(N)):  # time stepping: update from timestep t_j to j+1
@@ -72,19 +77,20 @@ def MLLGfuncsparse(T, N, tau, h, m, rho, L, tolgmres, eps, mu, sig, theta, alpha
 
         start6 = timeit.default_timer()
         
-        dtMex = interpolate(Expression(dtexSolutionCode,multF=3,tend=10*T,t=(j)*tau,degree=1),V3)
-        Mex = interpolate(Expression(exSolutionCode,multF=3,tend=10*T,t=(j+1)*tau,degree=1),V3)
+        #exact solution
+        #dtMex = interpolate(Expression(dtexSolutionCode,multF=3,tend=10*T,t=(j)*tau,degree=1),V3)
+        #Mex = interpolate(Expression(exSolutionCode,multF=3,tend=10*T,t=(j+1)*tau,degree=1),V3)
         
         (v, lam) = TrialFunctions(VV)
         (phi, mue) = TestFunctions(VV)
         lhs1 = ((alpha * inner(v, phi) + inner(cross(mj, v), phi) + Ce * tau * theta * inner(nabla_grad(v), nabla_grad(phi))) * dx + inner(dot(phi, mj), lam) * dx + inner(dot(v, mj), mue) * dx)
-        rhs1 = (-Ce * inner(nabla_grad(mj), nabla_grad(phi)) )*dx
+        rhs1 = (-Ce * inner(nabla_grad(mj), nabla_grad(phi)) )*dx #+(onof*inner(Hj, phi) + onof*inner(Hex, phi))*dx
         
         if (refadd<1.0):
             rhs1 = rhs1 +(inner(alpha*dtMex + cross(Mex,dtMex), phi) +Ce*inner(nabla_grad(Mex),nabla_grad(phi)))*dx
         if (onof >0.0 ):
             #lhs1= lhs1
-            rhs1 = rhs1 +(onof*inner(Hj, phi) - onof*(tau*(j))**3/3*inner(Hex, phi))*dx
+            rhs1 = rhs1 +(onof*inner(Hj, phi) + onof*inner(Hex, phi))*dx
         
         # compute solution
         vlam = Function(VV)
@@ -94,8 +100,8 @@ def MLLGfuncsparse(T, N, tau, h, m, rho, L, tolgmres, eps, mu, sig, theta, alpha
         # update magnetization
         (v, lam) = vlam.split(deepcopy=True)
         mj.vector()[:] = mj.vector()[:] + tau * v.vector()[:]
-        #mj = mj / sqrt(dot(mj, mj))
-        #mj = project(mj, V3, solver_type='cg')
+        mj = mj / sqrt(dot(mj, mj))
+        mj = project(mj, V3, solver_type='cg')
 
         stop6 = timeit.default_timer()
         print(' Time for LLG step                 ', j, ': ', stop6 - start6)
@@ -111,7 +117,7 @@ def MLLGfuncsparse(T, N, tau, h, m, rho, L, tolgmres, eps, mu, sig, theta, alpha
         boundary_rhs[0].coefficients[:] = np.zeros(nBEM)
         boundary_rhs[1].coefficients[:] = np.zeros(nBEM)
 
-        for kk in range(1, j +2):  # Convolution, start bei 1, da psiko(0)=0
+        for kk in range(1, j +2):  # Convolution, start with index 1, because psiko(0)=0 by definition
             dtmfunc[0].coefficients[:] = -dtmpsiko[kk,nBEM:]
             dtmfunc[1].coefficients[:] = np.sqrt(mu*eps)**(-1) *dtmpsiko[kk,:nBEM]
             boundary_rhs += rho ** (-(j + 1 - kk)) / L *np.real(wei[0] *dtmfunc)
@@ -123,11 +129,11 @@ def MLLGfuncsparse(T, N, tau, h, m, rho, L, tolgmres, eps, mu, sig, theta, alpha
 
         ##### Right hand side  #####
         t=(j+1)*tau
-        Rhs = np.concatenate([eps / tau * M0 * Ej.vector() +1.0/3.0*t**3*M0.dot(JccE.vector())+t*(sig*t+2.0)*M0.dot(JE.vector()),#- ((j+1)*tau )**2 * M0 * J.vector(),
-                              mu / tau * M0 * Hj.vector() - onof * mu * rhsLLGH.transpose() * (v.vector()-dtMex.vector()),
+        Rhs = np.concatenate([eps / tau * M0 * Ej.vector()[:] +1.0/3.0*t**3*M0.dot(JccE.vector())+t*(sig*t+2.0)*M0.dot(JE.vector()),#- ((j+1)*tau )**2 * M0 * J.vector(),
+                              mu / tau * M0 * Hj.vector()[:] - onof * mu * rhsLLGH.transpose() * (v.vector()[:]),
                               - 1.0 * np.real(boundary_rhs[0].projections(wei[0].dual_to_range_spaces[0])), #das projections projeziert nicht, sonern macht das richtige, mit der wform multiplizieren
                               - 1.0 * np.real(boundary_rhs[1].projections(wei[0].dual_to_range_spaces[1]))])  # - stimmt, da drei minus: unser B=-B, auf andere seite, und -1 durchmultipliziert
-
+#-dtMex.vector() 
         stop4 = timeit.default_timer()
         print(' Time for Convolution in time step ', j, ': ', stop4 - start4)
 
@@ -138,16 +144,19 @@ def MLLGfuncsparse(T, N, tau, h, m, rho, L, tolgmres, eps, mu, sig, theta, alpha
         def count_iterations(x):
             nonlocal it_count
             it_count += 1
-
-        sol, info = gmres(Lhs, Rhs, tol=tolgmres, callback=count_iterations, x0=sol)
-
+        
+        
+        sol = scipy.linalg.solve(Lhs, Rhs)#, tol=tolgmres,callback=count_iterations, x0=sol)
+        info=0
+        #sol, info = gmres(Lhs, Rhs, tol=tolgmres, callback=count_iterations,  x0=sol)
+        stop5 = timeit.default_timer()
+        print(' Time for gmres                    ', j, ': ', stop5 - start5)
         if (info > 0):
             print("Failed to converge after " + str(info) + " iterations")
         else:
             print("Solved system " + str(j) + " in " + str(it_count) + " iterations." )
 
-        stop5 = timeit.default_timer()
-        print(' Time for gmres                    ', j, ': ', stop5 - start5)
+        
 
         #### Storage and end of loop ###################
         Ej.vector()[:] = sol[:nFEM]  # coefficients for next timestep
@@ -158,17 +167,37 @@ def MLLGfuncsparse(T, N, tau, h, m, rho, L, tolgmres, eps, mu, sig, theta, alpha
         Hkoeff[j+1, :] = np.real(Hj.vector()[:])
         randkoeff[j + 1, :] = np.real(sol[2 * nFEM:])
 
+        mplot.rename("m", "m") #see the QA reported below.
+        mplot.vector()[:]= mkoeff[j+1,:]
+        vtkfilem << mplot, j+1
+        Eplot.rename("E", "E") #see the QA reported below.
+        Eplot.vector()[:]= Ekoeff[j+1,:]
+        vtkfileE << Eplot, j+1
+        
+        Hplot.rename("H", "H") #see the QA reported below.
+        Hplot.vector()[:]= Hkoeff[j+1,:]
+        vtkfileH << Hplot, j+1
+        
+        #Phiplot = bempp.api.GridFunction(trace_space, coefficients=phi12ko[:])
+        #vtkfilePhi << Phiplot, j+1
+        #Phiplot.plot()
+        #from exportfun import export
+        #export('grid_functionPhi.msh', grid_function=Phiplot, write_binary=False)
+        #Psiplot = bempp.api.GridFunction(trace_space, coefficients=-psiko[:])
+        #export('grid_functionPsi.msh', grid_function=Psiplot, write_binary=False)
+        
     return (mkoeff, Ekoeff, Hkoeff, randkoeff[:, :nBEM], randkoeff[:, nBEM:2 * nBEM])
 
-def assLHS( tau, h, m, eps, mu, sig,rho,L,N):
+def assign_data_and_systems( tau, h, m, eps, mu, sig,rho,L,N,thick):
     start = timeit.default_timer()
-    meshh = UnitCubeMesh(h, h, h)
+    meshh = BoxMesh(Point(0,0,0),Point(thick,thick,1),int(h),int(h),int(h/thick))#UnitCubeMesh(h, h, h)
 
     def dlt(z):
         # BDF1
         return 1.0 - z
         # BDF2
         # return 1.0-z+0.5*(1.0-z)**2
+        
     # approximaion spaces
     # m   = Lagrange1
     # E,H = N1curl
@@ -193,15 +222,16 @@ def assLHS( tau, h, m, eps, mu, sig,rho,L,N):
     nFEM = X.dim()
     nMAG = V3.dim()
 
-
+    # initial data and input data 
     (mj,Ej,Hj,JccE,JE)= initialdata(X,V3)
-
-    (Lhs,M0,rhsLLGH,boundary_rhs,dtmfunc)=assLHS2(X,V3,mu,trace_matrix,dlt,m,trace_space,eps,tau,sig,nFEM,nBEM)
+    
+    # left hand side matrix and matrices to build right hand side 
+    (Lhs,M0,rhsLLGH,boundary_rhs,dtmfunc)=assign_LHS(X,V3,mu,trace_matrix,dlt,m,trace_space,eps,tau,sig,nFEM,nBEM)
 
     stop = timeit.default_timer()
 
-
-    wei= assCQw(L,tau,m,mu,eps,trace_space.grid,dlt,rho,N,stop,start)
+    # Convolution quadrature weights
+    wei= assign_CQweights(L,tau,m,mu,eps,trace_space.grid,dlt,rho,N,stop,start)
 
     return (Lhs,rhsLLGH, M0,mj,Ej,Hj,JccE,JE,nBEM,nFEM,nMAG,X,trace_space,VV,V3,wei,boundary_rhs,dtmfunc)
 
@@ -334,16 +364,36 @@ def initialdata(X,V3):
     #mj = mj / sqrt(dot(mj, mj))
     #mj = project(mj, V3, solver_type='cg')
     Ej = project(Expression(['0.00', '0.0', '0.0'], degree=1), X)#interpolate(Jinit, X)#
-    Hj = project(Expression(['0.0', '0.0', '0.0'], degree=1), X)#
-    JccE = MyExpressionccE(degree=1)
-    JccE = interpolate(JccE, X)
+    Hj = project(Expression(['0.0', '-10.0', '0.0'], degree=1), X)#
+    #JccE = MyExpressionccE(degree=1)
+    JccE = project(Expression(('0.0', '0.0', '0.0'), degree=1), X)#interpolate(JccE, X)
     #dolfin.plot(J)
     #plt.show()
-    JE = MyExpressionE(degree=1)
-    JE=interpolate(JE, X)#project(Expression(['0.00','000.0','00.0'],degree=1),X)
+    #JE = MyExpressionE(degree=1)
+    JE=project(Expression(('0.0', '0.0', '0.0'), degree=1), X)# interpolate(JE, X)#project(Expression(['0.00','000.0','00.0'],degree=1),X)
+    
+    class MyExpressionM(UserExpression):
+        def eval(self, value, x):
+            #value[0] = 0.0
+            #value[1] = -1.0
+            #value[2] = 0.0
+            #if np.abs(x[2]-0.5)<0.25:# and (x[1]-0.05)**2+(x[0]-0.05)**2< 0.049**2:
+            x=x[2]
+            #x= (1-np.cos(np.pi*x))/2
+            s3=np.sin(2*np.pi*x)
+            c3=np.cos(2*np.pi*x)
+            value[0] = s3
+            value[1] = c3
+            value[2] = 0.0
+
+        def value_shape(self):
+                return (3,)
+    mj = interpolate(MyExpressionM(degree=3), V3)
+    Hj = project(Expression(['0.0', '00.0', '0.0'], degree=1), X)
+    
     return(mj,Ej,Hj,JccE,JE)
 
-def assLHS2(X,V3,mu,trace_matrix,dlt,m,trace_space,eps,tau,sig,nFEM,nBEM):
+def assign_LHS(X,V3,mu,trace_matrix,dlt,m,trace_space,eps,tau,sig,nFEM,nBEM):
     bc_space = bempp.api.function_space(trace_space.grid, "BC", 0)  # domain spaces
     rwg_space = bempp.api.function_space(trace_space.grid, "RWG", 0)
     snc_space = bempp.api.function_space(trace_space.grid, "SNC", 0)  # dual to range spaces
@@ -394,10 +444,10 @@ def assLHS2(X,V3,mu,trace_matrix,dlt,m,trace_space,eps,tau,sig,nFEM,nBEM):
     blocke1[3, 1] = 0.5 * massbd * trace_op
     blocke1[3, 3] = -mu * np.sqrt(eps / mu) *caldfac* cald[1, 0]
 
-    Lhs = BlockedDiscreteOperator(np.array(blocke1))
+    Lhs = assmatrix(BlockedDiscreteOperator(np.array(blocke1)))
     return(Lhs,M0,rhsLLGH,boundary_rhs,dtmfunc)
 
-def assCQw(L,tau,m,mu,eps,trace_spacegrid,dlt,rho,N,stop,start):
+def assign_CQweights(L,tau,m,mu,eps,trace_spacegrid,dlt,rho,N,stop,start):
     start2 = timeit.default_timer()
     # Definition of Convolution Quadrature weights
     storblock = np.ndarray([2, 2], dtype=np.object)  # dummy variable
@@ -415,3 +465,7 @@ def assCQw(L,tau,m,mu,eps,trace_spacegrid,dlt,rho,N,stop,start):
 
     return wei
 
+def assmatrix(operator):
+    from numpy import eye
+    cols = operator.shape[1]
+    return operator @ eye(cols)
